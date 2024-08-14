12265f21982fe018c56713168f47e6fe6dc611aa
alana-cbrl <alanacabralsilva1@gmail.com>
2024-08-14 16:22:13 -0300

    ícone colocado e botão funcionando

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -14,7 +14,11 @@ sap.ui.define([
       oRouter.navTo("Second");
     },
 
-
+    onNavToMain: function () {
+      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      oRouter.navTo("main");
+  },
+  
     onBotoDeAlertaButtonPress: function() {
       sap.m.MessageToast.show("Alerta!");
     },
@@ -25,5 +29,51 @@ sap.ui.define([
               var bExpanded = oSideNavigation.getExpanded();
               oSideNavigation.setExpanded(!bExpanded);
           },
+
+
+
+  
+  
+  handleEditPress: function () {
+    var oModel = this.getView().getModel();
+    oModel.setProperty("/editable", true); 
+    var oEditButton = this.getView().byId("edit");
+    var oSaveButton = this.getView().byId("save");
+    var oCancelButton = this.getView().byId("cancel");
+
+    
+    oEditButton.setVisible(false);
+    oSaveButton.setVisible(true);
+    oCancelButton.setVisible(true);
+},
+
+handleSavePress: function () {
+    var oModel = this.getView().getModel();
+    oModel.setProperty("/editable", false); 
+    var oEditButton = this.getView().byId("edit");
+    var oSaveButton = this.getView().byId("save");
+    var oCancelButton = this.getView().byId("cancel");
+  
+    oEditButton.setVisible(true);
+    oSaveButton.setVisible(false);
+    oCancelButton.setVisible(false);
+},
+
+
+
+handleCancelPress: function () {
+    var oModel = this.getView().getModel();
+    oModel.setProperty("/editable", false); 
+    var oEditButton = this.getView().byId("edit");
+    var oSaveButton = this.getView().byId("save");
+    var oCancelButton = this.getView().byId("cancel");
+
+    oEditButton.setVisible(true);
+    oSaveButton.setVisible(false);
+    oCancelButton.setVisible(false);
+}
+
+
+
   });
 });
