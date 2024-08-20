73d81d756cacd07a5a6d26fc6fa77b897ede9d3b
alana-cbrl <alanacabralsilva1@gmail.com>
2024-08-20 13:29:23 -0300

    códigos organizados

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,79 +1,79 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
+"sap/ui/core/mvc/Controller"
 ], function (Controller) {
-  "use strict";
+"use strict";
 
-  return Controller.extend("myApp.controller.Detail", {
-    onNavBack: function () {
-      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-      oRouter.navTo("Main");
-    },
-
-    onNavSecond: function () {
-      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-      oRouter.navTo("Second");
-    },
-
-    onNavToMain: function () {
-      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-      oRouter.navTo("main");
+return Controller.extend("myApp.controller.Detail", {
+  onNavBack: function () {
+    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+    oRouter.navTo("Main");
   },
+
+  onNavSecond: function () {
+    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+    oRouter.navTo("Second");
+  },
+
+  onNavToMain: function () {
+    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+    oRouter.navTo("main");
+},
+
+  onBotoDeAlertaButtonPress: function() {
+    sap.m.MessageToast.show("Alerta!");
+  },
+        onSideNavButtonPress: function(oEvent) {
+          
+            var oSideNavigation = this.byId("sideNavigation");
+          
+            var bExpanded = oSideNavigation.getExpanded();
+            oSideNavigation.setExpanded(!bExpanded);
+        },
+
+
+
+
+
+handleEditPress: function () {
+  var oModel = this.getView().getModel();
+  oModel.setProperty("/editable", true); 
+  var oEditButton = this.getView().byId("edit");
+  var oSaveButton = this.getView().byId("save");
+  var oCancelButton = this.getView().byId("cancel");
+
   
-    onBotoDeAlertaButtonPress: function() {
-      sap.m.MessageToast.show("Alerta!");
-    },
-          onSideNavButtonPress: function(oEvent) {
-           
-              var oSideNavigation = this.byId("sideNavigation");
-            
-              var bExpanded = oSideNavigation.getExpanded();
-              oSideNavigation.setExpanded(!bExpanded);
-          },
-
-
-
-  
-  
-  handleEditPress: function () {
-    var oModel = this.getView().getModel();
-    oModel.setProperty("/editable", true); 
-    var oEditButton = this.getView().byId("edit");
-    var oSaveButton = this.getView().byId("save");
-    var oCancelButton = this.getView().byId("cancel");
-
-    
-    oEditButton.setVisible(false);
-    oSaveButton.setVisible(true);
-    oCancelButton.setVisible(true);
+  oEditButton.setVisible(false);
+  oSaveButton.setVisible(true);
+  oCancelButton.setVisible(true);
 },
 
 handleSavePress: function () {
-    var oModel = this.getView().getModel();
-    oModel.setProperty("/editable", false); 
-    var oEditButton = this.getView().byId("edit");
-    var oSaveButton = this.getView().byId("save");
-    var oCancelButton = this.getView().byId("cancel");
-  
-    oEditButton.setVisible(true);
-    oSaveButton.setVisible(false);
-    oCancelButton.setVisible(false);
+  var oModel = this.getView().getModel();
+  oModel.setProperty("/editable", false); 
+  var oEditButton = this.getView().byId("edit");
+  var oSaveButton = this.getView().byId("save");
+  var oCancelButton = this.getView().byId("cancel");
+
+  oEditButton.setVisible(true);
+  oSaveButton.setVisible(false);
+  oCancelButton.setVisible(false);
 },
 
 
 
 handleCancelPress: function () {
-    var oModel = this.getView().getModel();
-    oModel.setProperty("/editable", false); 
-    var oEditButton = this.getView().byId("edit");
-    var oSaveButton = this.getView().byId("save");
-    var oCancelButton = this.getView().byId("cancel");
+  var oModel = this.getView().getModel();
+  oModel.setProperty("/editable", false); 
+  var oEditButton = this.getView().byId("edit");
+  var oSaveButton = this.getView().byId("save");
+  var oCancelButton = this.getView().byId("cancel");
 
-    oEditButton.setVisible(true);
-    oSaveButton.setVisible(false);
-    oCancelButton.setVisible(false);
+  oEditButton.setVisible(true);
+  oSaveButton.setVisible(false);
+  oCancelButton.setVisible(false);
 }
 
 
 
-  });
+});
 });
