7f8d8343d9edb86b4b89ea1eeae584c92410a151
alana-cbrl <alanacabralsilva1@gmail.com>
2024-10-04 21:24:12 -0300

    indentação

--- webapp/component.js
+++ webapp/component.js
@@ -1,24 +1,20 @@
-sap.ui.define([
-"sap/ui/core/UIComponent",
-"sap/ui/Device",
-"myApp/model/models"
-], function (UIComponent, Device, models) {
-"use strict";
+sap.ui.define(
+  ["sap/ui/core/UIComponent", "sap/ui/Device", "myApp/model/models"],
+  function (UIComponent, Device, models) {
+    "use strict";
 
-return UIComponent.extend("myApp.Component", {
-    metadata: {
-        manifest: "json"
-    },
+    return UIComponent.extend("myApp.Component", {
+      metadata: {
+        manifest: "json",
+      },
 
-    init: function () {
-    
+      init: function () {
         UIComponent.prototype.init.apply(this, arguments);
 
-        
         this.getRouter().initialize();
 
-
         this.setModel(models.createDeviceModel(), "device");
-    }
-});
-});
+      },
+    });
+  }
+);
